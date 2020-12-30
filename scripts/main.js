a564d6b68d60160e7788d1879316b42ac908e6e0
Jahongir95 <sjahongir0032@gmail.com>
2020-12-30 16:02:17 +0500

    three image slide show is created

--- scripts/main.js
+++ scripts/main.js
@@ -2,12 +2,13 @@ var mySwiper = new Swiper('.swiper-container', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
-    slidesPerView: 5,
+    slidesPerView: 3,
     spaceBetween: 30,
     centeredSlides: true,
   
     // If we need pagination
     pagination: {
       el: '.swiper-pagination',
+      clickable: true,
     },
   })
